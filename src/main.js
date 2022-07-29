6acc31054b9d9e999f340d3b3ae3b6871333bf4a
Leonardo Henao <leohenao2705@gmail.com>
2022-07-29 12:44:01 -0500

    Axios magration

--- src/main.js
+++ src/main.js
@@ -1,3 +1,67 @@
+// Axios
 
+// Con axios podemos traer todo directamente en una sola creación, así mismo podemos utilizar los headers y los parámetros como en este caso, trayendo nuestra API_KEY
 
-alert("hola")
+const api = axios.create({
+  baseURL: "https://api.themoviedb.org/3/",
+  headers: {
+    "Content-Type": "application/json;charset=utf-8",
+  },
+  params: {
+    'api_key': API_KEY, // aquí en forma de parámetros estaríamos enviando nuestra API_KEY, sin necesidad de citarla en cada función que queramos crear
+  },
+});
+
+async function getTrendingMoviesPreview() {
+  const { data } = await api('trending/movie/day');
+  const movies = data.results;
+
+  movies.forEach((movie) => {
+    const trendingPreviewMoviesContainer = document.querySelector(
+      "#trendingPreview .trendingPreview-movieList"
+    );
+
+    const movieContainer = document.createElement("div");
+    movieContainer.classList.add("movie-container");
+
+    const movieImg = document.createElement("img");
+    movieImg.classList.add("movie-img");
+    movieImg.setAttribute("alt", movie.title);
+    movieImg.setAttribute(
+      "src",
+      `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
+    );
+
+    movieContainer.appendChild(movieImg);
+    trendingPreviewMoviesContainer.appendChild(movieContainer);
+  });
+  // console.log({data, movies})
+}
+
+async function getCategoriesPreview() {
+  const { data } = await api('genre/movie/list');
+  const categories = data.genres;
+	
+  categories.forEach((category) => {
+    const previewCategoriesContainer = document.querySelector(
+      "#categoriesPreview .categoriesPreview-list"
+    );
+
+    const categoryContainer = document.createElement("div");
+    categoryContainer.classList.add("category-container");
+
+    const categoryTitle = document.createElement("h3");
+    categoryTitle.classList.add("category-title");
+    categoryTitle.setAttribute("id", "id" + category.id);
+    const categoryTitleText = document.createTextNode(category.name);
+
+    categoryTitle.appendChild(categoryTitleText);
+    categoryContainer.appendChild(categoryTitle);
+    previewCategoriesContainer.appendChild(categoryContainer);
+  });
+  // console.log({data, movies})
+}
+getTrendingMoviesPreview();
+getCategoriesPreview();
+
+// Axios es excelente por la facilidad de lectura del código
